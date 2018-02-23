4bd8d31c5e73846e63fb5a9451b429fb8a701da3
Ravindra Rathore <ravindra@whizkidzmedia.com>
2018-02-23 14:40:56 +0530

    Initial Commit

---
+++ MyContact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyContact
+//
+//  Created by Apple on 23/02/18.
+//  Copyright © 2018 Ravindra_Rathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
